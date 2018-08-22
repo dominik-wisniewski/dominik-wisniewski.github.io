697205beaf6cea30ecea3d547832444b6aff4456
dominik-wisniewski <42099601+dominik-wisniewski@users.noreply.github.com>
2018-08-22 21:29:53 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 
+$(function() {
+	$('ul.nav a').bind('click',function(event){
+		var $anchor = $(this);
+		/*
+		Easing effects:
+		$('html, body').stop().animate({
+			scrollLeft: $($anchor.attr('href')).offset().left
+		}, 1500,'easeInOutExpo');
+		 */
+		$('html, body').stop().animate({
+			scrollLeft: $($anchor.attr('href')).offset().left
+		}, 1000);
+		event.preventDefault();
+	});
+});
